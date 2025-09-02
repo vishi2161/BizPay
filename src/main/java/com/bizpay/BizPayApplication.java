7abef81b16c26b8239d708845d6a330a3581cdfb
ektasingh <e.singh161294@gmail.com>
2025-09-02 19:18:24 +0530

    initial commit

---
+++ src/main/java/com/bizpay/BizPayApplication.java
@@ -0,0 +1,13 @@
+package com.bizpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizPayApplication.class, args);
+	}
+
+}
